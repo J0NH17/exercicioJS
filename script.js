30861d635945520958a04240a506f3d7eae64709
J0NH17 <1091771@senacminas.edu.br>
2026-09-18 21:57:33 -0300

    javascript funcionando

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-let nome;
-let servico;
+   
+function orcamento(){
+    let servicoNome = "";
+    let valor = 0;
 
-function apresentar(nome){
-    "Olá "+nome;
-}
-function orcamento(servico){
+    let nome = document.getElementById("nome").value;
+    let serv = document.getElementById("servico").value;
+    
+    
+    document.getElementById("saudar").innerHTML = "Olá "+ nome;
+
+    
+    if (serv == "formatar"){
+        servicoNome = "Formatação de Computadores"; 
+        valor = 120;
         
+    }else if (serv == "limpar"){
+        servicoNome = "Limpeza e Manutenção"; 
+        valor = 80;
+        
+    }else if (serv == "instalar"){
+        servicoNome = "Instalação de Programas"; 
+        valor = 50;
+        
+    }else if (serv == "salvar"){
+        servicoNome = "Backup de Dados"; 
+        valor = 100;
+        
+    }else{
+        valor = 0;
+    }
+    
+    document.getElementById("serviconome").innerHTML = "Serviço escolhido: "+servicoNome;
+    document.getElementById("valorServico").innerHTML = "Valor: "+valor;
 }
