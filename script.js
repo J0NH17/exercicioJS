7a59c8db2354b231f5dc26af80c5ae21a5265688
J0NH17 <jonatha17052005@gmail.com>
2026-09-19 17:18:29 -0300

    Adicionado comentários e favincon no topo da página.

--- script.js
+++ script.js
@@ -1,35 +1,29 @@
-   
-function orcamento(){
-    let servicoNome = "";
-    let valor = 0;
+function orcamento() {
+  let servicoNome = "";
+  let valor = 0;
+  // Obtém o valor do campo de entrada de nome
+  let nome = document.getElementById("nome").value;
+  let serv = document.getElementById("servico").value;
 
-    let nome = document.getElementById("nome").value;
-    let serv = document.getElementById("servico").value;
-    
-    
-    document.getElementById("saudar").innerHTML = "Olá "+ nome;
-
-    
-    if (serv == "formatar"){
-        servicoNome = "Formatação de Computadores"; 
-        valor = 120;
-        
-    }else if (serv == "limpar"){
-        servicoNome = "Limpeza e Manutenção"; 
-        valor = 80;
-        
-    }else if (serv == "instalar"){
-        servicoNome = "Instalação de Programas"; 
-        valor = 50;
-        
-    }else if (serv == "salvar"){
-        servicoNome = "Backup de Dados"; 
-        valor = 100;
-        
-    }else{
-        valor = 0;
-    }
-    
-    document.getElementById("serviconome").innerHTML = "Serviço escolhido: "+servicoNome;
-    document.getElementById("valorServico").innerHTML = "Valor: "+valor;
+  document.getElementById("saudar").innerHTML = "Olá " + nome + " !";
+  
+  // Verifica o serviço selecionado e define o nome do serviço e o valor correspondente
+  if (serv == "formatar") {
+    servicoNome = "Formatação de Computadores";
+    valor = 120;
+  } else if (serv == "limpar") {
+    servicoNome = "Limpeza e Manutenção";
+    valor = 80;
+  } else if (serv == "instalar") {
+    servicoNome = "Instalação de Programas";
+    valor = 50;
+  } else if (serv == "salvar") {
+    servicoNome = "Backup de Dados";
+    valor = 100;
+  } else {
+    valor = 0;
+  }
+  // Atualiza o conteúdo dos elementos HTML com o nome do serviço escolhido e o valor correspondente
+  document.getElementById("serviconome").innerHTML ="Serviço escolhido: " + servicoNome;
+  document.getElementById("valorServico").innerHTML = "Valor : R$" + valor;
 }
